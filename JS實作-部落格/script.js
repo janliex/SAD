ad564687167a12ac8d405e625e6856843c38c3c5
class-2022 <40971203H@gapps.ntnu.edu.tw>
2023-05-11 21:06:59 +0800

    new

--- JS實作-部落格/script.js
+++ JS實作-部落格/script.js
@@ -4,15 +4,26 @@ var content = document.getElementById('content');
 var btn = document.getElementById('btn'); 
 var list = document.getElementById('list'); 
 
-btn.addEventListener("clcik",function(){
-    list.innerHTML = list.innerHTML +
+btn.addEventListener("click",function(){
+    list.innerHTML = list.innerHTML + ` 
     <div class="article">
-         <h2>$(title.value)</h2> 
-         <p>$(content.value)</p>
+        <h2>${title.value}</h2> 
+        <p>${content.value}</p>
     </div>
-    ; //-模板語法-
+    `  ;
     //上傳之後再清空
+    //-模板語法-
     title.value ='';
-    content.value=''
+    content.value='';
 
 })
+
+var btn = document.getElementById('btn');
+ btn.addEventListener("mouseover",function(){ 
+    this.style.color = 'CornflowerBlue';
+ })
+
+ var btn = document.getElementById('btn');
+ btn.addEventListener("mouseout",function(){ 
+    this.style.color = 'black';
+ })
